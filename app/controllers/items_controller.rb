1975cf4c09718bb925a24ce91a3a8261c7c65374
yuki-o8616 <0ku387431pf3g8d@ezweb.ne.kp>
2020-11-03 19:41:54 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit,:update, :destroy ]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order(created_at: 'DESC')
