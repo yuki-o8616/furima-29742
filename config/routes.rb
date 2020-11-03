6fd0d5644933ad179f9e718ef42e1e45f91cb339
yuki-o8616 <0ku387431pf3g8d@ezweb.ne.kp>
2020-11-03 19:51:19 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   #   post 'sending_destinations', to: 'users/registrations#createSendingDestination'
   # end
   root 'items#index'
-  resources :categories, only: [:index, :show] 
+  
   
   resources :items 
 
